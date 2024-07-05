9cee8c3d358801ac5ffd19213bad0f1156bcba7d
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2024-07-05 21:33:07 +0200

    Feat: add template function with multiple definitions, ex01 done

---
+++ ex01/include/iter.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 15:00:07 by sebasnadu         #+#    #+#             */
+/*   Updated: 2024/07/05 20:47:41 by sebasnadu        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <cstddef>
+#include <iostream>
+
+template <typename T, typename TFunction>
+void iter(T *array, size_t len, TFunction fn) {
+  for (size_t i = 0; i < len; i++)
+    fn(array[i]);
+}
+
+template <typename T> void printArray(T const *array, size_t len) {
+  if (len) {
+    std::cout << array[0];
+    for (size_t i = 1; i < len; i++)
+      std::cout << ", " << array[i];
+    std::cout << '.';
+  }
+  std::cout << std::endl;
+}
+
+template <typename T> void multiplyByTwo(T &n) { n *= 2; }
+
+template <typename T> void addOne(T &n) { n += 1; }
+
+template <> void addOne(std::string &str) { str += "1"; }
---
+++ ex01/src/main.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 16:01:40 by sebasnadu         #+#    #+#             */
+/*   Updated: 2024/07/05 20:42:50 by sebasnadu        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "colors.hpp"
+#include "iter.hpp"
+#include <iostream>
+#include <string>
+
+static inline void upperChar(char &c) { c = std::toupper(c); }
+
+static void upperString(std::string &str) {
+  for (std::string::iterator it = str.begin(); it != str.end(); it++)
+    *it = std::toupper(*it);
+}
+
+int main(void) {
+  std::cout << YELLOW "\n--------Testing with int array-------\n" RESET;
+  int intArr[] = {1, 2, 3, 4, 5};
+  size_t len = 5;
+
+  std::cout << "Original array: " GREEN;
+  ::printArray<int>(intArr, len);
+  ::iter<int>(intArr, len, ::addOne<int>);
+  std::cout << RESET "Array after iter with addOne: " GREEN;
+  ::printArray<int>(intArr, len);
+  ::iter<int>(intArr, len, ::multiplyByTwo<int>);
+  std::cout << RESET "Array after iter with multiplyByTwo: " GREEN;
+  ::printArray<int>(intArr, len);
+
+  std::cout << YELLOW "\n--------Testing with double array-------\n" RESET;
+  double doubleArr[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+  std::cout << "Original array: " GREEN;
+  ::printArray<double>(doubleArr, len);
+  ::iter<double>(doubleArr, len, ::addOne<double>);
+  std::cout << RESET "Array after iter with addOne: " GREEN;
+  ::printArray<double>(doubleArr, len);
+  ::iter<double>(doubleArr, len, ::multiplyByTwo<double>);
+  std::cout << RESET "Array after iter with multiplyByTwo: " GREEN;
+  ::printArray<double>(doubleArr, len);
+
+  std::cout << YELLOW "\n--------Testing with char array-------\n" RESET;
+  char charArr[] = {'a', 'b', 'c', 'd', 'e'};
+  std::cout << "Original array: " GREEN;
+  ::printArray<char>(charArr, len);
+  ::iter<char>(charArr, len, ::addOne<char>);
+  std::cout << RESET "Array after iter with addOne: " GREEN;
+  ::printArray<char>(charArr, len);
+  ::iter<char>(charArr, len, upperChar);
+  std::cout << RESET "Array after iter with upperChar: " GREEN;
+  ::printArray<char>(charArr, len);
+
+  std::cout << YELLOW "\n--------Testing with string array-------\n" RESET;
+  std::string strArr[] = {"hello", "world", "this", "is", "42"};
+  std::cout << "Original array: " GREEN;
+  ::printArray<std::string>(strArr, len);
+  ::iter<std::string>(strArr, len, ::addOne<std::string>);
+  std::cout << RESET "Array after iter with addOne: " GREEN;
+  ::printArray<std::string>(strArr, len);
+  ::iter<std::string>(strArr, len, upperString);
+  std::cout << RESET "Array after iter with upperString: " GREEN;
+  ::printArray<std::string>(strArr, len);
+  return 0;
+}
